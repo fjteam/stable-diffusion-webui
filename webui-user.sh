542ead6a9b7bd47e10c485cbb72acb15b8a23f77
tony <tony@zuzuche.com>
2023-10-08 07:44:31 +0000

    1

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--listen --xformers --opt-split-attention --no-half-vae --no-gradio-queue --enable-insecure-extension-access"
+export COMMANDLINE_ARGS="--listen --api --xformers --opt-split-attention --no-half-vae --no-gradio-queue --enable-insecure-extension-access"
 
 # python3 executable
 #python_cmd="python3"
